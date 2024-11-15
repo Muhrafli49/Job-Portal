9ffc4fd1580218b7281b341ea7698c677496b4cf
Rafli <mraflie49@gmail.com>
2024-11-15 19:22:09 +0700

    Update model CompanyJob

--- app/Models/CompanyJob.php
+++ app/Models/CompanyJob.php
@@ -25,7 +25,7 @@ class CompanyJob extends Model
     ];
 
     public function category(){
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class, 'category_id');
     }
 
     public function company(){
