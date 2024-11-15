05b34bd5740a4f372e1cea581728c4fc99494112
Rafli <mraflie49@gmail.com>
2024-11-15 19:24:33 +0700

    Update route in web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\CompanyJobController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobCandidateController;
 use App\Models\JobCandidate;
 use Illuminate\Support\Facades\Route;
@@ -20,9 +21,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/details/{company_jobs:slug}', [HomeController::class, 'details'])->name('home.details');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -34,6 +34,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::prefix('dashboard')->name('dashboard.')->group(function () {
+        
         Route::middleware('can:apply job')->group(function () {
             Route::get('my-applications', [DashboardController::class, 'my_applications'])->name('my.applications');
             Route::get('my-applications/{job_candidate}', [DashboardController::class, 'my_application_details'])->name('my.application.details');
@@ -45,19 +46,19 @@ Route::middleware('auth')->group(function () {
         Route::middleware('can:manage categories')->group(function () {
             Route::resource('categories', CategoryController::class);
         });
-        
+
         Route::middleware('can:manage company')->group(function () {
             Route::resource('company', CompanyController::class);
         });
-        
+
         Route::middleware('can:manage jobs')->group(function () {
             Route::resource('company_jobs', CompanyJobController::class);
         });
-        
+
         Route::middleware('can:manage applicants')->group(function () {
             Route::resource('job_candidates', JobCandidateController::class);
             Route::get('/candidate/{job_candidate}/resume/download', [JobCandidateController::class, 'download_file'])->name('download_resume');
-        });        
+        });
     });
 });
 
