678d31c9eac08206372e157c4f3699387fb60079
Rafli <mraflie49@gmail.com>
2024-11-18 20:03:58 +0700

    add job application feature

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreApplyJobRequest;
 use App\Models\Category;
 use App\Models\CompanyJob;
+use App\Models\JobCandidate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -24,5 +28,39 @@ class HomeController extends Controller
         ->get();
         
         return view('home.details', compact('companyJob', 'jobs'));
+    }
+
+    public function apply(CompanyJob $companyJob) {
+        return view('home.apply', compact('companyJob'));
+    }
+
+    public function apply_store(StoreApplyJobRequest $request, CompanyJob $companyJob){
+        $user = Auth::user();
+
+        $hasApplied = JobCandidate::where('company_job_id', $companyJob->id)->where('candidate_id', $user->id)->first();
+        if($hasApplied){
+            return redirect()->back()->withErrors(['applied' => 'Failed! You have already applied for this job.']);
+        }
+
+        DB::transaction(function() use ($request, $user, $companyJob) {
+            $validated = $request->validated();
+
+            if($request->hasFile('resume')){
+                $resumePath = $request->file('resume')->store('resumes/' . date('Y/m/d'), 'public');
+                $validated['resume'] = $resumePath;
+            }
+
+            $validated['candidate_id'] = $user->id;
+            $validated['is_hired'] = false;
+            $validated['company_job_id'] = $companyJob->id;
+
+            $newData = JobCandidate::create($validated);
+        });
+
+        return redirect()->route('home.apply.success');
+    }
+
+    public function success_apply(){
+        return view('home.success_apply');
     }
 }
